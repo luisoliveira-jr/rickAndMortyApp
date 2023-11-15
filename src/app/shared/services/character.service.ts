d64a5a053fb72121cdd6cc80d061504ccfe62f0f
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-11-15 00:33:47 -0300

    Create character service

---
+++ src/app/shared/services/character.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { Character } from '../interfaces/character.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CharacterService {
+  constructor(private http: HttpClient) {}
+
+  searchCharacters(query = '', page = 1) {
+    const filter = `${environment.baseUrlAPI}/?name=${query}&page=${page}`;
+    return this.http.get<Character[]>(filter);
+  }
+
+  getDetails(id: number) {
+    return this.http.get<Character>(`${environment.baseUrlAPI}/${id}`);
+  }
+}
