5061a6ee1b809ccdb44618c02874555e74845dfe
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-11-15 07:06:45 -0300

    update character module

--- src/app/components/pages/characters/characters.module.ts
+++ src/app/components/pages/characters/characters.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterDetailsComponent } from './character-details/character-details.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
   declarations: [
-
+    CharacterListComponent,
+    CharacterDetailsComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    InfiniteScrollModule,
+  ],
+  exports:[
+    CharacterListComponent,
+    CharacterDetailsComponent,
   ]
 })
 export class CharactersModule { }
