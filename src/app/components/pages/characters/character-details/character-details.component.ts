b392c312228299752796fea38e897d2428d687e9
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-11-15 07:06:29 -0300

    update character details

--- src/app/components/pages/characters/character-details/character-details.component.ts
+++ src/app/components/pages/characters/character-details/character-details.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { CharacterService } from '@app/shared/services/character.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Character } from '@app/shared/interfaces/character.interface';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
-  styleUrls: ['./character-details.component.scss']
+  styleUrls: ['./character-details.component.scss'],
 })
-export class CharacterDetailsComponent {
+export class CharacterDetailsComponent implements OnInit {
+  character$: Observable<Character> | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private characterservice: CharacterService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.pipe(take(1)).subscribe((params) => {
+      const id = params['id'];
+      this.character$ = this.characterservice.getDetails(id);
+    });
+  }
+
+  onGoBack():void{
+    this.location.back();
+  }
+
 
 }
