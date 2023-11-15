bbd125797c3b983dd5a59cf33e0aac5070a24c51
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-11-14 23:51:16 -0300

    update header and form search

--- src/app/shared/components/form-search/form-search.component.ts
+++ src/app/shared/components/form-search/form-search.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-search',
   templateUrl: './form-search.component.html',
-  styleUrls: ['./form-search.component.scss']
+  styleUrls: ['./form-search.component.scss'],
 })
-export class FormSearchComponent {
+export class FormSearchComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {};
+
+  onSearch(value: string) {
+    console.log('Buscar->', value)
+    if (value && value.length > 3) {
+      this.router.navigate(['/character-list'], {
+        queryParams: { query: value },
+      });
+    }
+  }
 }
